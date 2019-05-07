74732ee9c2a40c3ffa1d2d21ca2e3e268593a254
Kendall Katelynn <kkatelyn@vo-a1.21-school.ru>
2019-05-07 16:55:10 +0300

    1

--- main.c
+++ main.c
@@ -6,80 +6,57 @@
 /*   By: kkatelyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 12:47:28 by kkatelyn          #+#    #+#             */
-/*   Updated: 2019/05/06 17:43:07 by kkatelyn         ###   ########.fr       */
+/*   Updated: 2019/05/07 16:54:57 by kkatelyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fil.h"
 
-int		ft_filchr(char *line)
+int		lstoper(t_list **new, char *tt, int nt)
 {
-	int i;
-	int a;
-	int b;
-	int c;
-	int d;
-	int	e;
-	int f[3];
+	t_list *tmp;
 
-	a = b = c = d = 0;
-	i = -1;
-	while (line[++i])
-	{	
-		
-
-		if (i == 0 && line[i] == '#')
-			a = i;
-		if (i == 1 && line[i] == '#')
-			b = i;
-		if (i == 2 && line[i] == '#')
-			c = i;
-		if (i == 3 && line[i] == '#')
-			d = i;
-		if (i == 4 && line[i] == '\n')
-			e = i;
-		if (i == 5 && line[i])
-			return (-1);
-	}
-	f = {a, b, c, d};
-}
-
-int		check(int fd)
-{
-	char *line;
-	int a;
-
-	while ()
+	tmp = *new;
+	while (tmp)
 	{
-		a = get_next_line(fd, &line);
-		ft_filchr(line);
+		if ((int)tmp->content_size = nt)
+			break ;
+		tmp = tmp->next;
 	}
-	else
-		ft_putstr(line);
-	return (0);
+	ft(
+	tmp
 }
 
-int		fil(int fd)
+int		fil(int fd, int a, char *line)
 {
-	int a;
-	char *tt;
-	char *tmp;
-
-	CHECK(tt = ft_strnew(1));
+	char	*tt;
+	char	*tm;
+	int		i;
+	int		nt;
+	t_list	*new;
+	
+	CHECK(tt = ft_strnew(0));
+	i = -1;
+	nt = 0;
 	while ((a = get_next_line(fd, &line)))
 	{
 		if (a == -1)
-		{
-			free(tt);
-			return (-1);
-		}
-		CHECK(tmp = ft_strjoin(tt, line));
+			return (-1);//++free all possible existing
+		tm = ft_strjoin(tt, line);//+add free
 		free(tt);
-		tt = tmp;
+		tt = tm;
+		while (tt[++i])
+		{
+			if (i == 15 && tt[i] == '\n')
+			{
+				nt++;
+				lstoper(&new, tt, nt);
+			}
+			if (nt > 26)
+				return (-1);//++free APE
+
+		}
 	}
-	while (tt[a])
-		a = 0 - 3 = '.' / '\n' 4 = '\n'
-		if [5] musst '\n' and then if [6]
 	return (-1);
 }
 
@@ -87,12 +64,16 @@ int		main(int ac, char **av)
 {
 	int fd;
 	int st;
+	int a;
+	char *line;
 
 	if (ac == 2)
 	{
 		if ((fd = open(av[1], O_RDONLY)) == -1)
 			ERROR;
-		st = fil(fd);
+		a = 0;
+		line = NULL;
+		st = fil(fd, a, line);
 		if (close(fd) == -1 || st == -1)
 			ERROR;
 		return (0);
