9086ff2c9487c1b2be5a8d7ff6ec58d0d666f9b3
Kendall Katelynn <kkatelyn@vo-a1.21-school.ru>
2019-05-10 20:40:33 +0300

    1

--- main.c
+++ main.c
@@ -6,23 +6,40 @@
 /*   By: kkatelyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 12:47:28 by kkatelyn          #+#    #+#             */
-/*   Updated: 2019/05/08 18:13:42 by kkatelyn         ###   ########.fr       */
+/*   Updated: 2019/05/10 20:40:22 by kkatelyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fil.h"
-#define TRX (x + 1) < 5
-#define TRY (y + 1) < 5
-#define TRXN (x - 1) > -1
-#define TRYN (y - 1) > -1
-#define FX (x - 1) < 0
-#define FY (y - 1) < 0
-#define XP [x + 1][y] != '#'
+
+int		conmi(char *tt)
+{
+	int	i;
+	int fl;
+
+	i = 0;
+	fl = 0;
+	while (tt[i])
+	{
+		if (tt[i] == '#')
+		{
+			if ((i + 1) < 16 && tt[i + 1] == '#')
+				fl = i + 1;
+			if ((i + 4) < 16 && tt[i + 4] == '#')
+				fl = i + 4;
+			else if ((fl - 1) >= 0 && tt[fl - 1] == '#')
+				fl = fl - 1;
+		}
+		i++;
+	}
+}
 
 int		lstoper(t_list **new, char *tt, int nt)
 {
-	t_list *tmp;
+	t_list	*tmp;
+	int		s;
 
+	s = conmi(tt);
 	tmp = *new;
 	while (tmp)
 	{
@@ -37,85 +54,49 @@ int		lstoper(t_list **new, char *tt, int nt)
 	ft_lstadd(left, tmp);
 }
 
-int		chmtrx(char **mtrx)
+int		checkmatet(char *tt, int *nt)
 {
-	int x;
-	int y;
-	int
-	x = 0;
-	y = 0;
-	while (y < 4)
-	{
-		while (x < 4)
+	int		i;
+	int		t;
+	char	*tmp;
+	t_list	*new;
+
+	tet = 0;
+	i = -1;
+	while (tt[++i])
+		if (i == 15)
 		{
-			if (mtrx[x][y] == '#')
-			{
-				if (TRX(x) && TRY(y) && TRXN(x) && TRYN(y))
-					if (XP(mtrx) || mtrx[x][y + 1] != '#' ||
-								mtrx[x - 1][y] != '#' || mtrx[x][y - 1] != '#')
-						return (-1);
-				if ((x + 1) < 5 && (y + 1) < 5 && (x - 1) < 0 && (y - 1) < 0)
-					if (mtrx[x + 1][y] != '#' || mtrx[x][y + 1] != '#')
-						return (-1);
-				if ((
-					//check all around
-				//then check radius like +x +y and if possible -x -y. -x || -y != < 0
-			x++;
+			*nt++;
+			if (nt > 26)
+				return (-1);//+free
+			t = flstoper(&new, tt, nt);
 		}
-		y++;
-	}
-}
-
-char	*joinmtrx(char const *s1, char const *s2, int nt)
-{
-	char **b;
-
-	if (!(b = (char**)malloc(sizeof(char*) * (5))))
-		return (NULL);
+	return (0);
 }
 
 int		fil(int fd, int a, char *line)
 {
 	char	*tt;
-	char	*tm;
-	int		i;
 	int		nt;
-	int		tet;
-	t_list	*new;
-	
+	char	*tmp;
+	int		i;
+
 	CHECK(tt = ft_strnew(0));
-	i = -1;
 	nt = 0;
 	while ((a = get_next_line(fd, &line)))
 	{
+		i = -1;
 		if (a == -1)
-			return (-1);//++free all possible existing
+			return (-1);//+free line and maybe other
+		CHECK(tmp = ft_strjoin(*tt, line));//+add free
+		free(*tt);
+		*tt = tmp;
 		while (line[++i])
-		{
-			if (line[i] == '#')
-				tet++;
 			if (i > 3 || line[i] != '.' || line[i] != '#')
 				return (-1);//+free
-		}
-		tm = ft_strjoin(tt, line);//+add free
-		free(tt);
-		tt = tm;
-		if (tet > 4)
-			return (-1);
-		while (tt[++i])
-			if (i == 15)
-			{
-				if (tet < 4)
-					return (-1);//free
-				nt++;
-				lstoper(&new, tt, nt);
-				i = 0;
-				tet = 0;
-			}
-		if (nt > 26)
-			return (-1);//++free APE
+		a = checkmatet(&tt, &nt);
+		free(line);
 	}
-	return (-1);
 }
 
 int		main(int ac, char **av)
